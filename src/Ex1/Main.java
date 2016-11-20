730bae32d40123cbbe561a9f093c1a1791af3211
Sergey <java.courses.2016@gmail.com>
2016-11-21 00:25:28 +0200

    Ex1 commit

--- src/Ex1/Main.java
+++ src/Ex1/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.print("Введіть розмір масиву: "); //код в порядке, но пользователь должен понимать, что от него хотят.
         Scanner sors = new Scanner(System.in);
         int n = sors.nextInt();
         outputArray(inputArray(n));
@@ -15,14 +16,20 @@ public class Main {
 
         int[] arr = new int[n];
         for (int i = 0; i < arr.length; i++) {
+            System.out.print("arr["+ i +"] = ");
             arr[i] = scanner.nextInt();
         }
         return arr;
     }
 
     public static  void outputArray(int[] ar){
+        System.out.print("arr: ");
         for (int i = 0; i < ar.length; i++) {
-            System.out.print(ar[i] + ", ");
+
+            System.out.print(ar[i]);
+            if (i < ar.length-1){
+                System.out.print(", ");//Убрана запятая после вывода массива.
+            }
         }
 
     }
