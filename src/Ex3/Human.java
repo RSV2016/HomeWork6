53639248e8f47d88003afed396ef69b6f1bafb7d
Sergey <java.courses.2016@gmail.com>
2016-11-21 00:59:46 +0200

    Ex3 commit

--- src/Ex3/Human.java
+++ src/Ex3/Human.java
@@ -1,12 +1,13 @@
 package Ex3;
 
 public class Human {
-
     String name;
     String secondName;
     int age;
 
-
-
-
+    void printInfo(){
+        System.err.println("Name: " + this.name);
+        System.err.println("SecondName: " + this.secondName);
+        System.err.println("Age: " + this.age);
+    }
 }
